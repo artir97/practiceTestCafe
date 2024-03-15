5b9a977e379a50ab2ece505c9adcf60c4ef90af4
artir97 <85441675+artir97@users.noreply.github.com>
2024-03-15 23:23:30 +0100

    ADD: playing around with testcafe 2

--- tests/actions.demo.test.js
+++ tests/actions.demo.test.js
@@ -1,5 +1,8 @@
+const { Selector } = require("testcafe");
+
 fixture('actions demo tests')
     .page('https://trytestingthis.netlify.app/');
+
 
 test('TetsCafe actions', async t => {
     await t
@@ -34,6 +37,48 @@ test('TetsCafe actions', async t => {
         .click('body > div.row > div.side.ex1 > div.pop-up-alert > button')
         .wait(2000)
 
-        // Screenshot
-        // .takeScreenshot('screenshot1.png')
+    // Screenshot
+    // .takeScreenshot('screenshot1.png')
 })
+
+
+fixture.only('actions demo tests 2')
+    .page('https://trytestingthis.netlify.app/');
+
+
+test('TestCafe Actions 2', async t => {
+    const dropDown = Selector('#option');
+    const dropDownOptions = dropDown.find('option');
+
+    await t
+        // Dropdown
+        .click(dropDown)
+        .click(dropDownOptions.withText('Option 2'))
+        .expect(dropDown.value).eql('option 2')
+        .wait(1000)
+        // Upload a file 
+        .setFilesToUpload('#myfile', ['./uploads/abc.txt','./uploads/def.txt'])
+        .wait(1000)
+        // .click('#upload-btn') if there is an upload button you can access it this way
+        // Resize browser window
+        .resizeWindow(500,500)
+        .wait(500)
+        .resizeWindow(1000,1000)
+        .wait(500)
+        .maximizeWindow()
+        // Scroll Page
+        .scrollBy(0, 500)
+        .wait(500)
+        .scrollBy(0,500)
+        .wait(500)
+        .scrollBy(0, -500)
+        .wait(500)
+        .scrollBy(0, -500)
+
+        // Assertions
+        .expect(Selector('body > div.navbar > a:nth-child(2)').innerText).eql('Home')
+
+        // Navigate to
+        .wait(2000)
+        .navigateTo('https://google.de')
+    })
